57b6fb3bf76f3d130cfdc63ecb93e95258350f59
Oliver Gomes <ogomesphoto@gmail.com>
2019-01-28 02:34:13 -0500

    styling update to cal

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {
   Text,
   TextInput,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  ImageBackground
 } from "react-native";
 
 export default class App extends React.Component {
@@ -34,42 +35,61 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.intro}>
-          <TextInput
-            placeholder="Height"
-            keyboardType="numeric"
-            style={styles.input}
-            onChangeText={height => {
-              this.setState({ height });
+      <ImageBackground
+        source={require("./assets/bg.png")}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <View style={styles.container}>
+          <Text
+            style={{
+              color: "#FFCB1F",
+              justifyContent: "center",
+              alignSelf: "center",
+              marginTop: 30,
+              fontSize: 15
             }}
-          />
-          <TextInput
-            placeholder="Mass"
-            keyboardType="numeric"
-            style={styles.input}
-            onChangeText={mass => {
-              this.setState({ mass });
-            }}
-          />
-        </View>
+          >
+            BMI Calculator
+          </Text>
+          <View style={styles.intro}>
+            <TextInput
+              placeholder="Height"
+              keyboardType="numeric"
+              style={styles.input}
+              onChangeText={height => {
+                this.setState({ height });
+              }}
+            />
+            <TextInput
+              placeholder="Mass"
+              keyboardType="numeric"
+              style={styles.input}
+              onChangeText={mass => {
+                this.setState({ mass });
+              }}
+            />
+          </View>
 
-        <TouchableOpacity style={styles.button} onPress={this.handleCalculate}>
-          <Text style={styles.buttonText}>Calculator</Text>
-        </TouchableOpacity>
-        <Text style={styles.result}>{this.state.resultNumber}</Text>
-        <Text style={[styles.result, { fontSize: 35 }]}>
-          {this.state.resultText}
-        </Text>
-      </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.handleCalculate}
+          >
+            <Text style={styles.buttonText}>Calculate</Text>
+          </TouchableOpacity>
+          <Text style={styles.result}>{this.state.resultNumber}</Text>
+          <Text style={[styles.result, { fontSize: 35 }]}>
+            {this.state.resultText}
+          </Text>
+        </View>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#f5fcff"
+    flex: 1
+    // backgroundColor: "#f5fcff"
   },
   intro: {
     flexDirection: "row"
@@ -79,21 +99,22 @@ const styles = StyleSheet.create({
     textAlign: "center",
     width: "50%",
     fontSize: 50,
-    marginTop: 24
+    marginTop: 24,
+    color: "#FFCB1F"
   },
   button: {
-    backgroundColor: "#89ffa5"
+    backgroundColor: "#1D1D1B"
   },
   buttonText: {
     alignSelf: "center",
     padding: 30,
     fontSize: 25,
-    color: "#6dc4a4",
+    color: "#FFCB1F",
     fontWeight: "bold"
   },
   result: {
     alignSelf: "center",
-    color: "lightgray",
+    color: "#FFCB1F",
     fontSize: 65,
     padding: 15
   }
